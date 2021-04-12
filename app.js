3e691c215a169ddf360336e18df100cf086d325e
이승태 <fo_ma@naver.com>
2021-04-12 14:04:47 +0900

    Express routing

---
+++ app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import { userRouter } from "./router";
+const app = express();
+
+const handleHome = (req, res) => res.send("Hello from my ass");
+
+const handleProfile = (req, res) => res.send("You are on my profile");
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
+app.use("/user", userRouter);
+
+export default app;
