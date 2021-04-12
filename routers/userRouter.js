29b5c2c6c59e56115e711959a56b7cef129d4f30
이승태 <fo_ma@naver.com>
2021-04-12 18:05:50 +0900

    Install pug

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -8,9 +8,8 @@ import {
 } from "../controllers/userControllers";
 const userRouter = express.Router();
 
-userRouter.get(routes.users, users);
-userRouter.get(routes.userDetail, userDetail);
 userRouter.get(routes.editProfile, editProfile);
 userRouter.get(routes.changePassword, changePassword);
+userRouter.get(routes.userDetail, userDetail);
 
 export default userRouter;
