cf275a64e66c7af95bf95973cc8f2f36b1aae4d5
이승태 <fo_ma@naver.com>
2021-04-12 17:04:26 +0900

    set controlls

--- app.js
+++ app.js
@@ -2,22 +2,21 @@ import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
-import { userRouter } from "./router";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
+
 const app = express();
 
-const handleHome = (req, res) => res.send("Hello from my ass");
-
-const handleProfile = (req, res) => res.send("You are on my profile");
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.get("/", handleHome);
-
-app.get("/profile", handleProfile);
-
-app.use("/user", userRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
